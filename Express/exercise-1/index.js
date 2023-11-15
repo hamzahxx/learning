4b51a32708d6b4b0e96a3eb2073fc42fb03a549a
Hamzah <hamzahchhapra2004@gmail.com>
2023-11-15 19:45:45 +0530

    Exercise-1 Complete

--- Express/exercise-1/index.js
+++ Express/exercise-1/index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Dude Learning Backend!")
-})
+    res.send("<h1>Hello Dude Learning Backend!</h1>")
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me!</h1>")
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1>")
+});
 
 app.listen(port, () => console.log(`suckcesfully started the server on ${port}`));

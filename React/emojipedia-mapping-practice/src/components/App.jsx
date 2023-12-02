860088a3c6bde5e124fa57426a16fc39c303122a
Hamzah <hamzahchhapra2004@gmail.com>
2023-12-02 13:24:18 +0530

    emoji-pedia (unfinished transfer)

---
+++ React/emojipedia-mapping-practice/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="heading">Emojipedia</h1>
+      </div>
+      <div className="card">
+        <h1></h1>
+        <h1>\/</h1>
+        <h2>Heart</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;

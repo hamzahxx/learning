fa957fe2b4bbdafcafb3e681c343b2308cda1413
Hamzah <hamzahchhapra2004@gmail.com>
2023-12-05 02:31:53 +0530

    completion of ES6 updations on another project

--- React/emojipedia-mapping-practice/src/components/App.jsx
+++ React/emojipedia-mapping-practice/src/components/App.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import shopping from "../gottaBuy";
 import Card from "./Card";
 
-function createCard(data) {
-  return (
-    <Card
-      key={data.id}
-      item={data.item}
-      name={data.name}
-      meaning={data.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <>
       <h1 className="transition duration-300 ease-in-out pb-1 px-2 inline bg-sky-300 text-center text-5xl text-white font-bold p-{heading}">
         emojipedia
       </h1>
-      <div className="p-5 lg:flex">{shopping.map(createCard)}</div>
+      <div className="p-5 lg:flex">
+        {shopping.map((data) => (
+          <Card
+            key={data.id}
+            item={data.item}
+            name={data.name}
+            meaning={data.meaning}
+          />
+        ))}
+      </div>
     </>
   );
 }

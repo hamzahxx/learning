c8689d24ed79d9d7ed63fa86fff0517abb858c34
Hamzah <hamzahchhapra2004@gmail.com>
2023-12-09 21:43:00 +0530

    React states completion

---
+++ React/states-1/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App() {
+  const [isDone, setIsDone] = useState(false);
+
+  function strike() {
+    setIsDone(true);
+  }
+
+  function unStrike() {
+    setIsDone(false);
+  }
+  return (
+    <div>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
+  );
+}
+
+export default App;

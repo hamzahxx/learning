100c779076705041cfca6ee8f5d1cb112ac54c15
Hamzah <hamzahchhapra2004@gmail.com>
2024-03-25 17:20:17 +0530

    Added Practice for props and children

--- React/props-practice/src/components/App.jsx
+++ React/props-practice/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import PropChildren from "./PropChildren";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Card arr={contacts[0]} />
       <Card arr={contacts[1]} />
       <Card arr={contacts[2]} />
+      <PropChildren>BADA </PropChildren>
     </div>
   );
 }
---
+++ React/props-practice/src/components/PropChildren.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PropChildren(props) {
+    return <h1> {props.children} BAZINGA</h1>
+}
+
+export default PropChildren

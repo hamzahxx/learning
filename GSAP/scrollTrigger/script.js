89d4bf54b5f11c061640f69320d08cda30ab7333
Hamzah <hamzahchhapra2004@gmail.com>
2024-06-07 14:18:31 +0530

    scrubbing and pinning

--- GSAP/scrollTrigger/script.js
+++ GSAP/scrollTrigger/script.js
@@ -1,29 +1,3 @@
-// gsap.from("#page1 #box", {
-//   scale: 0,
-//   delay: 1,
-//   duration: 2,
-//   rotate: 360,
-// });
-
-// gsap.from("#page2 #box", {
-//   scale: 0,
-//   duration: 2,
-//   rotate: 360,
-//   scrollTrigger: "#page2 #box",
-// });
-
-// gsap.from("#page3 #box", {
-//   scale: 0,
-//   duration: 2,
-//   rotate: 360,
-//   scrollTrigger: {
-//     trigger: "#page3 #box",
-//     scroller: "body",
-//     markers: true,
-//     start: "top 60%",
-//   },
-// });
-
 gsap.from("#page2 h1", {
   opacity: 0,
   duration: 2,
@@ -59,8 +33,9 @@ gsap.from("#page3 #box", {
   scrollTrigger: {
     trigger: "#page3 #box",
     scroller: "body",
-    start: "top 60%",
+    start: "top 50%",
     end: "top 30%",
-    scrub: true,
-  }
-})
+    scrub: 2,
+    pin: true,
+  },
+});

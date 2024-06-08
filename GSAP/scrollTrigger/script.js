45659e2fa624b2334a794fda48757fb31a7fea47
Hamzah <hamzahchhapra2004@gmail.com>
2024-06-08 19:30:23 +0530

    wethinkelastic-EXPERTISES

--- GSAP/scrollTrigger/script.js
+++ GSAP/scrollTrigger/script.js
@@ -1,40 +1,10 @@
-gsap.from("#page2 h1", {
-  opacity: 0,
-  duration: 2,
-  x: -500,
+gsap.to("#page2 h1", {
+  transform: "translateX(-55%)",
   scrollTrigger: {
-    trigger: "#page2 h1",
+    trigger: "#page2",
     scroller: "body",
-    start: "top 40% ",
-  },
-});
-
-gsap.from("#page2 h2", {
-  opacity: 0,
-  duration: 2,
-  x: 500,
-  scrollTrigger: {
-    trigger: "#page2 h2",
-    scroller: "body",
-    start: "top 60% ",
-  },
-});
-
-gsap.from("#page1 #box", {
-  scale: 0,
-  duration: 2,
-  rotate: 720,
-});
-
-gsap.from("#page3 #box", {
-  scale: 0,
-  duration: 2,
-  rotate: 720,
-  scrollTrigger: {
-    trigger: "#page3 #box",
-    scroller: "body",
-    start: "top 50%",
-    end: "top 30%",
+    start: "top 0%",
+    end: "top -100%",
     scrub: 2,
     pin: true,
   },
